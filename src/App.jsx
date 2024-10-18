238aaa4cba284c1dba79b22606556fef1289bdad
andrewkozinski <andrewkozinski76@gmail.com>
2024-10-18 17:07:12 -0400

    Added .env file and app.jsx. .env file is visible for codepath staff to see.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [dataList, setDataList] = useState([]);
+  const [totalCities, setTotalCities] = useState(0);
+  const [maxTemp, setMaxTemp] = useState(0);
+
+  const handleSearch = () => {
+
+  }
+
+  useEffect(() => {
+    // Fetch data from API
+    const fetchData = async () => {
+      const response = await fetch(`https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=${API_KEY}&include=minutely`);
+      const data = await response.json();
+      setDataList(data);
+      console.log(data);
+    }
+
+    fetchData().catch(console.error);
+  }
+  , []);
 
   return (
     <>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Search by city"
+      />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Total Cities Found: </p>
+        <p>Mean Temperature: °C</p>
+        <p>Max Temperature: °C</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
