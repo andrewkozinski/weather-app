fa11cead569211c9303e3a43f2ec9da27fbac054
andrewkozinski <andrewkozinski76@gmail.com>
2024-10-22 21:11:39 -0400

    Removed import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '98.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 
 createRoot(document.getElementById('root')).render(
