d941dbf0962021699e48be6e39748ea26ff45424
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-10-25 15:01:56 -0400

    Added window display to WeatherDetail.jsx

--- src/Components/WeatherDetail.jsx
+++ src/Components/WeatherDetail.jsx
@@ -1,7 +1,32 @@
+import {useParams} from "react-router-dom";
+import React from "react";
+
 const WeatherDetail = () => {
+
+    let params = useParams();
+
+
     return (
         <div>
             <h1>Weather Detail</h1>
+
+
+            <div className="window">
+      <div className="title-bar">
+        <div className="title-bar-text">Counter</div>
+        <div className="title-bar-controls">
+          <button aria-label="Minimize" />
+          <button aria-label="Maximize" />
+          <button aria-label="Close" />
+        </div>
+      </div>
+
+      <div className="window-body">
+      </div>
+    </div>
+
+
+
         </div>
     );
 }
